9ead7c3bba9c8051de3e9451739389ac7c6bf756
sskiragu <skiragu18@gmail.com>
2023-03-14 18:48:24 +0300

    Added Role

--- models/index.js
+++ models/index.js
@@ -10,4 +10,24 @@ db.sequelize = sequelize;
 import User from './User.js';
 db.User = User(sequelize, Sequelize);
 
+import Role from './Role.js';
+db.Role = Role( sequelize, Sequelize );
+
+//Create the relationship - User and Role
+//To create a One-To-One relationship, the hasOne and belongsTo associations are used together;
+// To create a One-To-Many relationship, the hasMany and belongsTo associations are used together;
+// To create a Many-To-Many relationship, two belongsToMany calls are used together.
+// Note: there is also a Super Many-To-Many relationship, which uses six associations at once, and will be discussed in the Advanced Many-to-Many relationships guide.
+db.Role.belongsToMany(db.User, {
+    through: "user_roles",
+    foreignKey: "role_id",
+    otherKey: "user_id"
+  });
+  
+  db.User.belongsToMany(db.Role, {
+    through: "user_roles",
+    foreignKey: "user_id",
+    otherKey: "role_id"
+  });
+  
 export default db;
