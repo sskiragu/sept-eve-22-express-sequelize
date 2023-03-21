528b901be82956ad67e4b987ba84486d59557dff
sskiragu <skiragu18@gmail.com>
2023-03-21 18:58:53 +0300

    added authRooutes & authControllers skelletons

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+//import the model
+import db from '../models/index.js';
+
+const User = db.User;
+
+const login = async (req, res) => {
+    
+}
+
+const logout = async (req, res) => {
+    
+}
+
+const signup = async (req, res) => {
+    
+}
+
+
+export { login, logout, signup };
