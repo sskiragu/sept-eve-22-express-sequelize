147dfd641c02ee31a731d644fc7720536543de30
sskiragu <skiragu18@gmail.com>
2023-03-22 18:51:42 +0300

    Signup added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 import userRoutes from './routes/userRoutes.js';
 import roleRoutes from './routes/roleRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import db from './models/index.js';
 
 //creating an instance of express
@@ -18,6 +19,7 @@ sequelize.sync()
 
 app.use("/api/v1",userRoutes);
 app.use("/api/v1", roleRoutes);
+app.use('/api/v1', authRoutes);
 
 //define port and hostname
 const port = process.env.PORT || 5000;
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,11 @@
+import bcrypt from 'bcrypt';
 //import the model
 import db from '../models/index.js';
 
 const User = db.User;
 
 const login = async (req, res) => {
-    
+    //generate token (jwt) after login
 }
 
 const logout = async (req, res) => {
@@ -12,7 +13,14 @@ const logout = async (req, res) => {
 }
 
 const signup = async (req, res) => {
-    
+    const { username, email, password } = req.body;
+    try {
+        const hash = await bcrypt.hash(password, 10);
+        User.create({ username: username, email: email, password: hash });
+        res.status(201).json({"message": "Signup successfull."})
+    } catch (error) {
+        res.status(401).json(error);
+    }
 }
 
 
